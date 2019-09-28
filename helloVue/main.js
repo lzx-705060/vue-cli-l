6be106042cba5620f0409e4f5db643e4c91d1d55
李志雄 <lizhixiong@lizhixiongdeMacBook-Pro.local>
2019-09-28 21:13:13 +0800

    李志雄 2019-9-28

---
+++ helloVue/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Mint '../../static/assets/mint.js'
+import MintCss from '../../static/assets/mint.css';
+import App from './App'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+    ...App
+})
+app.$mount()
---
+++ helloVue/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+  <div id="app">
+    <mt-button @click.native="handleClick">Button</mt-button>
+  </div>
+	</view>
+</template>
+
+
+<script>
+	// import {mint} from '../../static/assets/mint.js'
+	export default {
+		el:'#app',
+		data() {
+			return {
+				title: ' 你好'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+         handleClick: function() {
+	          this.$toast('Hello world!')
+	        }
+		}
+	}
+	
+</script>
+
+<style>
+	/* @import url("../../static/assets/mint.css"); */
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
